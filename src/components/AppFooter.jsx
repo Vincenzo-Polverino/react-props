0d6d1ec1fa25cd0d6cb7964478cfe59722b8637a
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-13 18:46:20 +0100

    add text in footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,7 +2,9 @@ export default function AppFooter() {
 
     return (
         <footer>
-
+            <div className="container">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo molestias quas corrupti? Quaerat ipsa at suscipit accusantium obcaecati itaque iusto mollitia veniam consectetur necessitatibus hic, illo voluptates aut voluptate molestias.</p>
+            </div>
         </footer>
     )
 }
