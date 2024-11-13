4b6e907d6bb98c2d1c7b141a3c2371101a312cba
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-13 15:31:19 +0100

    add header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,8 +1,10 @@
 export default function AppHeader() {
-
+    const appName = 'Il mio blog'
     return (
         <header>
-
+            <div className="container">
+                <h1>{appName}</h1>
+            </div>
         </header>
     )
 }
